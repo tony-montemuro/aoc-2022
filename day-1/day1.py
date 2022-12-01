f1416c32c3ede32d799b58b23defff7c8fafa7b5
Tony Montemuro <tonyamontemuro@gmail.com>
2022-11-30 23:13:59 -0600

    day 1 solution

--- day-1/day1.py
+++ day-1/day1.py
@@ -1,5 +1,24 @@
 # open file
 f = open("puzzle.txt", "r")
+calories = f.read().split('\n')
+
+# loop through list of calories, and find the sum of calories for each elf
+elves = []
+elf = 0
+for c in calories:
+    if c == '':
+        elves.append(elf)
+        elf = 0
+    else:
+        elf += int(c)
+elves.append(elf)
+
+# part 1: find the elf with most number of calories
+elves.sort(reverse=True)
+print(elves[0])
+
+# part 2: find the 3 elves with the most number of calories, and find sum
+print(elves[0]+elves[1]+elves[2])
 
 # close file
 f.close()

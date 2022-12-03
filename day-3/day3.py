2f8a1e1be2f63d6f7beb59bbbf7d6bee73951ab6
Tony Montemuro <tonyamontemuro@gmail.com>
2022-12-02 23:43:32 -0600

    cleaned up day 3 solution

--- day-3/day3.py
+++ day-3/day3.py
@@ -13,17 +13,10 @@ for i in range(1, 27):
 # the common item among both compartments
 items = []
 for sack in rucksacks:
-    set1, set2 = set(), set()
     mid = int(len(sack)/2)
-    c1, c2 = sack[0:mid], sack[mid:len(sack)]
-    for c in c1:
-        set1.add(c)
-    for c in c2:
-        set2.add(c)
-    for e1 in set1:
-        for e2 in set2:
-            if e1 == e2:
-                items.append(e1)
+    set1, set2 = set(sack[0:mid]), set(sack[mid:len(sack)])
+    common = list(set1.intersection(set2))[0]
+    items.append(common)
 
 # now, based on the priorities dictionary, determine the sum of all common items
 sum_1 = sum([priorities[item] for item in items])
@@ -33,19 +26,9 @@ print(sum_1)
 # the three rucksacks
 items = []
 for i in range(0, len(rucksacks)-1, 3):
-    set1, set2, set3 = set(), set(), set()
-    s1, s2, s3 = rucksacks[i], rucksacks[i+1], rucksacks[i+2]
-    for c in s1:
-        set1.add(c)
-    for c in s2:
-        set2.add(c)
-    for c in s3:
-        set3.add(c)
-    for e1 in set1:
-        for e2 in set2:
-            for e3 in set3:
-                if e1 == e2 and e2 == e3:
-                    items.append(e1)
+    set1, set2, set3 = set(rucksacks[i]), set(rucksacks[i+1]), set(rucksacks[i+2])
+    common = list(set1.intersection(set2).intersection(set3))[0]
+    items.append(common)
 
 # now, based on the priorities dictionary, determine the sum of all common items
 sum_2 = sum([priorities[item] for item in items])

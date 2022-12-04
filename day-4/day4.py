eca6e9acbaace69d7e9624f1bebec4543e269dd7
Tony Montemuro <tonyamontemuro@gmail.com>
2022-12-03 23:40:24 -0600

    cleaned-up day 4 solution

--- day-4/day4.py
+++ day-4/day4.py
@@ -2,11 +2,11 @@
 f = open("puzzle.txt", "r")
 sections = f.read().split('\n')
 
-# go through each pair of sections, and count each time there is total overlap
+# go through each pair of sections, and count each time there is total overlap or partial overlap
 t1, t2 = 0, 0
 for section in sections:
     assignments = section.split(',')
-    first, second = assignments[0].split(',')[0].split('-'), assignments[1].split(',')[0].split('-')
+    first, second = assignments[0].split('-'), assignments[1].split('-')
     l1, u1 = int(first[0]), int(first[1])
     l2, u2 = int(second[0]), int(second[1])
     if l2 >= l1 and u1 >= u2 or l1 >= l2 and u2 >= u1:

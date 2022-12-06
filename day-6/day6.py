e137f62d57ba8887016b863347a0dda82a614b41
Tony Montemuro <tonyamontemuro@gmail.com>
2022-12-05 23:15:35 -0600

    cleaned-up day 6 solution

--- day-6/day6.py
+++ day-6/day6.py
@@ -1,33 +1,24 @@
+# find the first occurence of marker with length defined by length parameter
+def marker_finder(length):
+    buffer = data[0:length-1]
+    pos = length-1
+    found = False
+    while not found:
+        buffer += str(data[pos])
+        dupes = set(buffer)
+        if len(dupes) == length:
+            found = True
+        buffer = buffer[1:]
+        pos += 1
+    return pos
+
 # open file
 f = open("puzzle.txt", "r")
 data = f.readline()
 
-# find the first start-of-packet
-buffer = data[0:3]
-pos = 3
-found = False
-while not found:
-    buffer += str(data[pos])
-    dupes = set(buffer)
-    if len(dupes) == 4:
-        found = True
-    buffer = buffer[1:]
-    pos += 1
-print(pos)
-
-# find the first start-of-message
-buffer = data[0:13]
-pos = 13
-found = False
-while not found:
-    buffer += str(data[pos])
-    dupes = set(buffer)
-    print(dupes)
-    if len(dupes) == 14:
-        found = True
-    buffer = buffer[1:]
-    pos += 1
-print(pos)
+# print the first occurence of a marker of different lengths
+print(marker_finder(4))
+print(marker_finder(14))
 
 # close file
 f.close()
